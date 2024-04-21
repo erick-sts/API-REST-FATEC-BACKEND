1376e06cb98cde3e5ae6c3b3cb9035a0a51930bc
Erick Oliveira <erick.sts.eo@gmail.com>
2024-04-21 14:26:24 -0300

    atualizacao cors

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: 'https://piss-git-main-ericksts-projects.vercel.app'
+  }));
+  
 app.use('/professores', professorRoute);
 app.use('/usuarios', usuarioRoute);
 
